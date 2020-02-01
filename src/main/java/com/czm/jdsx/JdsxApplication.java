42b8478e70398b7fbbc7940a6b03638194e8e683
czmgithub9373 <czm-github>
2020-02-01 20:23:40 +0800

    git init

---
+++ src/main/java/com/czm/jdsx/JdsxApplication.java
@@ -0,0 +1,12 @@
+package com.czm.jdsx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class JdsxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdsxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/czm/jdsx/JdsxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.czm.jdsx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdsxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
